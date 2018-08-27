6ae961492825181495d155500f16e97538c319f7
B533P-20\533 <abc@hotmail.com>
2018-08-27 17:25:52 -0400

    Cours trois; Les balle ne tire pas dans la bonne direction

---
+++ Cours 3/Cours3/Assets/Script/Bullet.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+    Rigidbody rigidbody;
+	// Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+        Vector3 ajustedMouvement = transform.TransformDirection(Vector3.forward);
+       // rigidbody.AddForce(ajustedMouvement * 1000);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Cours 3/Cours3/Assets/Script/FireGun.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireGun : MonoBehaviour {
+    [SerializeField] GameObject bullet;
+    [SerializeField] float fireRate = 1;
+    [SerializeField] GameObject bulletEmetor;
+    private float timerLastShoot = 0;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timerLastShoot += Time.deltaTime;
+        if (timerLastShoot > fireRate)
+        {
+        var fire1 = Input.GetAxis("Fire1");
+        if (fire1 > 0)
+        {
+            Instantiate(bullet, bulletEmetor.transform.position, bulletEmetor.transform.rotation);
+                print(bulletEmetor.transform.rotation);
+                timerLastShoot = 0;
+        }
+        }
+		
+	}
+}
